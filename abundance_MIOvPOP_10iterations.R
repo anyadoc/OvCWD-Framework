200b3538eeaba442acaa50c143bb55e4f3fd9472
Aniruddha Belsare <belsare1@msu.edu>
2020-02-22 11:55:36 -0500

    Update abundance_MIOvPOP_10iterations.R

--- abundance_MIOvPOP_10iterations.R
+++ abundance_MIOvPOP_10iterations.R
@@ -10,12 +10,12 @@ data_github <- "https://raw.githubusercontent.com/anyadoc/OvCWD-Framework/master
 model_data <- read_csv(data_github)
 #model_data <- read.csv("data/saMontcalmCountyMI_10ite1Feb20.csv", header = TRUE)
 as_tibble(model_data)
-new_data <- add_column(model_data, year = rep(c(1:25), params$iterations))  # number of years per iteration 26, model iterations 100 #tibble
-new_data1 <- add_column(new_data, iteration = rep(c(1:params$iterations), each = 25))
+new_data <- add_column(model_data, year = rep(c(1:25), 10))  # number of years per iteration 26, model iterations 10 #tibble
+new_data1 <- add_column(new_data, iteration = rep(c(1:10), each = 25))
 new_data2 <- subset(new_data1, year == 25)
 MIDNR_est <- rnorm(10,51800,2500)   #deer abundance data generated based on MIDNR's estimate post-harvest estimate for Montcalm County (51800 +- 5%SD#2590)
 MIOvPOP <- new_data2$`pre-harvest_total`#model population data; pre.harvest_total for local file
-iteration <- c(1:params$iterations)
+iteration <- c(1:10)
 mydata3 <- cbind(MIDNR_est,MIOvPOP,iteration)
 mydata_df <- as.data.frame(mydata3)
 mydata3_long <- gather(mydata_df, data_source, deer_abundance, MIDNR_est:MIOvPOP, factor_key=TRUE)
